f8440160611844d03614cec4f032db83b7c61751
Ricardo0jsfniki <62771065+Ricardo0jsfniki@users.noreply.github.com>
2025-10-31 09:44:36 +0000

    Update app.go

--- node/api/app.go
+++ node/api/app.go
@@ -10,11 +10,12 @@ import (
 
 func Initialize( nodeCtx context.Context, ipfs iface.CoreAPI, pubSubService *messaging.PubSubService ){
 
-  app := gin.New();
-  app.Use(gin.Logger());
-  app.Use(gin.Recovery());
+  app := gin.New(); //a app está a criar instancia de um http server
+  app.Use(gin.Logger()); //é um middleware que vai dar logs das mensagens
+  app.Use(gin.Recovery()); 
 
+  //Vai utilizar as rotas e vai implementar no grupo, passa o context, ipfs e pubsubservice
   fileRoutes.SetUpRoutes(app.Group("/file"), nodeCtx, ipfs, pubSubService);
-  app.Run(":9000");
+  app.Run(":9000"); //A app vai correr na porta 9000
 }
 
