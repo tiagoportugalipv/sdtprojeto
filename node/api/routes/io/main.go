d104068d69edc7f81075c0794bc6900cae5d82d8
Ricardo0jsfniki <62771065+Ricardo0jsfniki@users.noreply.github.com>
2025-10-31 09:14:23 +0000

    Update main.go

--- node/api/routes/io/main.go
+++ node/api/routes/io/main.go
@@ -8,8 +8,10 @@ import (
  "sdt/node/services/messaging"
 )
 
+//Configura as rotas
 func SetUpRoutes(rg *gin.RouterGroup, nodeCtx context.Context, ipfs iface.CoreAPI, pubSubService *messaging.PubSubService){
 
+ //Um cliente envia uma requisição POST para /upload e o Gin corre a função passada
   rg.POST("/upload", func(c *gin.Context) {
     fileController.UploadFile(c,nodeCtx,ipfs,pubSubService)
   })
